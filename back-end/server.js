9bf2fa12765e1dd3af3846f900384b59107a0df1
James <jfnelson2000@gmail.com>
2023-04-22 14:14:22 -0700

    commented out mongoose test

--- back-end/server.js
+++ back-end/server.js
@@ -13,7 +13,7 @@ mongoose.connect("mongodb://localhost:27017/todo", {
     .then(() => console.log("Connected to Database"))
     .catch(console.error)
 
-const newTodo = new Todo({ text: "Test"})
-newTodo.save()
+// const newTodo = new Todo({ text: "Test"})
+// newTodo.save()
 
 app.listen(4000, ()=>{console.log('Server running on port 4000')})
