fb8f050ba5f173100acfb28a21320c460dc39861
James <jfnelson2000@gmail.com>
2023-04-24 20:09:51 -0700

    cleaned up server.js and added filler html

--- back-end/server.js
+++ back-end/server.js
@@ -15,16 +15,5 @@ mongoose.connect("mongodb://localhost:27017/todo", {
     .then(() => console.log("Connected to Database"))
     .catch(console.error)
 
-// routes and paths
-
-
-// testing
-async function run() {
-    const todo = await Todo.create({ text: "Code", done: true})
-    console.log(todo)
-}
-
-// run()
-
 // run server on port
 app.listen(4000, ()=>{console.log('Server running on port 4000')})
--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
     <div className="App">
-      
+      <h1>Welcome, User</h1>
+      <h3>To Do List:</h3>
+      <div className="list">
+        <div className="task">
+          Do the Laundry
+        </div>
+        <div className="task">
+          Buy groceries
+        </div>
+      </div>
     </div>
   );
 }
