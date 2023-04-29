73c7eed14aa31f9a643e71417d6f3b5fa44e3f70
James <jfnelson2000@gmail.com>
2023-04-29 10:19:55 -0700

    removed filler html

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,16 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Welcome, User</h1>
-      <h3>To Do List:</h3>
-      <div className="list">
-        <div className="task">
-          Do the Laundry
-        </div>
-        <div className="task">
-          Buy groceries
-        </div>
-      </div>
+
     </div>
   );
 }
